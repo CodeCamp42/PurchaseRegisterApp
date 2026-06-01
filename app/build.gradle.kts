72e95868a97220af4ff1084f994ca3fc6d2cc889
Eric Silva Paucar <ericsilvapaucar@gmail.com>
2026-06-01 16:18:10 -0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.rctm.rctm"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
